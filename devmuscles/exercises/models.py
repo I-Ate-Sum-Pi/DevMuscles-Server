4470960d651c5182eee7f854a047fa7f71e597d4
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-03 11:45:19 +0100

    refactored exercise model to its own app and corresponding model, added a home view, added that model to admin site

---
+++ devmuscles/exercises/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+class exercise(models.Model):
+    id = models.CharField(max_length=80, primary_key=True)
+    workout_id = models.CharField(max_length=300)
+    name = models.CharField(max_length=500)
+    reps = models.IntegerField()
+    weight = models.DecimalField(max_digits = 4, decimal_places=1)
---
+++ devmuscles/exercises/views.py
@@ -0,0 +1,7 @@
+from django import http
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def home(request):
+    return HttpResponse("exercies route working")
