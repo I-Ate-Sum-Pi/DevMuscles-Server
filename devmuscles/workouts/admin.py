ac239db00c5cda92d73552f1ea4e848c55d1a5a4
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-03 11:01:41 +0100

    added models, debuged models not showing on admin site

--- devmuscles/workouts/admin.py
+++ devmuscles/workouts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import date, exercise, user, workout
 # Register your models here.
+admin.site.register(workout)
+admin.site.register(user)
+admin.site.register(exercise)
+admin.site.register(date)
--- devmuscles/workouts/models.py
+++ devmuscles/workouts/models.py
@@ -1,26 +1,26 @@
 from django.db import models
 
 # Create your models here.
-class users(models.Model):
-    id = models.CharField(max_length=80)
+class user(models.Model):
+    id = models.CharField(max_length=80, primary_key=True)
     name = models.CharField(max_length=300)
     password = models.CharField(max_length=500)
     email = models.CharField(max_length=500)
 
-class workouts(models.Model):
-    id = models.CharField(max_length=80)
+class workout(models.Model):
+    id = models.CharField(max_length=80, primary_key=True)
     user_id = models.CharField(max_length=300)
     name = models.CharField(max_length=500)
 
-class exercises(models.Model):
-    id = models.CharField(max_length=80)
+class exercise(models.Model):
+    id = models.CharField(max_length=80, primary_key=True)
     workout_id = models.CharField(max_length=300)
     name = models.CharField(max_length=500)
     reps = models.IntegerField()
     weight = models.DecimalField(max_digits = 4, decimal_places=1)
 
-class dates(models.Model):
-    id = models.CharField(max_length=80)
+class date(models.Model):
+    id = models.CharField(max_length=80, primary_key=True)
     workout_id = models.CharField(max_length=300)
     user_id = models.CharField(max_length=500)
     date = models.CharField(max_length=10)
