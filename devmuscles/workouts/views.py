88a9a151fe2d6fc174cf42097eb14015b0a668ce
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-05 21:56:05 +0100

    added views for /workouts, WorkoutList class for displaying all workouts and posting a new one, also a WorkoutDetail class to view individual workouts and update them

--- devmuscles/workouts/views.py
+++ devmuscles/workouts/views.py
@@ -1,7 +1,41 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import Http404
+from rest_framework.views import APIView
 from .models import Workout
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from .serializers import WorkoutSerializer
 
 # Create your views here.
-def home(request):
-    return JsonResponse({"something": "workout route working"}, status=200)
+class WorkoutList(APIView):    
+    def get(self, request, format=None):
+            workouts = Workout.objects.all()
+            print(workouts)
+            serializer = WorkoutSerializer(workouts, many=True)
+            return Response(serializer.data)
+    
+    def post(self, request, format=None):
+        serializer = WorkoutSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class WorkoutDetail(APIView):
+    def get_object(self, workout_id):
+        try:
+            return Workout.objects.get(pk=workout_id)
+        except Workout.DoesNotExist:
+            raise Http404
+
+    def get(self, request, workout_id, format=None):
+        workout = self.get_object(workout_id)
+        serializer = WorkoutSerializer(workout)
+        return Response(serializer.data)
+
+    def put(self, request, workout_id, format=None):
+        workout = self.get_object(workout_id)
+        serializer = WorkoutSerializer(workout, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
