11f1928fb8ac3994bc7b28b2a30f2194121c922e
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-05 17:25:28 +0100

    installed djangorestframework

--- devmuscles/devmuscles/settings.py
+++ devmuscles/devmuscles/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'workouts.apps.WorkoutsConfig',
     'users.apps.UsersConfig',
     'exercises.apps.ExercisesConfig',
