2a4f57d78b9e885f0afd6f3996c2e0876c58937f
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-05 18:28:27 +0100

    mod user views to return serialized data for all users and also allow creating users via post request

--- devmuscles/users/views.py
+++ devmuscles/users/views.py
@@ -1,7 +1,20 @@
-from django import http
-from django.shortcuts import render
-from django.http import JsonResponse
-# Create your views here.
-def home(request):
-    return JsonResponse({"response":"user route is working"}, status=200)
+from rest_framework.views import APIView
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
 
+from .serializers import UserRegistrationSerializer, UserSerializer
+
+
+class UserList(APIView):
+    def get(self, request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
