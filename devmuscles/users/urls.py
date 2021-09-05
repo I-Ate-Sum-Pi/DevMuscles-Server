f86e5e4d39ca2a69dbc12e63125d549716714fee
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-05 18:27:27 +0100

    modified url path to show all users on users/ route

--- devmuscles/users/urls.py
+++ devmuscles/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import UserList
 
 urlpatterns = [
-    path('', views.home, name='users-home'),
+    path('', UserList.as_view(), name='all-users'),
 ]
