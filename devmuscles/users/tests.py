37746267722ccc37b44f13efd55546012f6996b5
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-06 17:02:45 +0100

    Basic test for users status code is now passing

--- devmuscles/users/tests.py
+++ devmuscles/users/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory, Client
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+
+class BaseTestCase(TestCase):
+    
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create_user(username = 'test', email='myemail@crazymail.com', password='mypassword')
+
+class TestWorkoutViews(BaseTestCase):
+    c = Client()
+
+    def test_status_code_is_200(self):
+        response = self.c.get(f'/users/{self.user.id}')
+        data = response.json()
+        print(response)
+        print(data)
+        assert response.status_code == 200
+
+
+            
