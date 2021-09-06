66d82a53d45bb1203c76441a1e58e02e33556f20
akashkhambay <akashkhambay@hotmail.co.uk>
2021-09-06 12:20:57 +0100

    Add url paths for dates and date id

--- devmuscles/users/urls.py
+++ devmuscles/users/urls.py
@@ -1,4 +1,5 @@
 from exercises.views import ExerciseList, ExerciseDetail
+from dates.views import DateList, DateDetail
 from workouts.views import userWorkouts, userWorkout
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
@@ -11,4 +12,9 @@ urlpatterns = [
     path('<int:user_id>/workouts/<int:workout_id>', userWorkout),
     path('<int:user_id>/workouts/<int:workout_id>/exercises', ExerciseList.as_view()),
     path('<int:user_id>/workouts/<int:workout_id>/exercises/<int:exercise_id>', ExerciseDetail.as_view()),
+    path('<int:user_id>/dates', DateList.as_view()),
+    path('<int:user_id>/dates/<int:date_id>', DateDetail.as_view()),
+
+
+    
 ]
