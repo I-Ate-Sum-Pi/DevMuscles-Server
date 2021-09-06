62df498e86ee3fee95af6c021319ae371f2824ef
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-06 10:54:31 +0100

    added get, put and delete method for individual exercises

--- devmuscles/exercises/views.py
+++ devmuscles/exercises/views.py
@@ -1,7 +1,7 @@
 from django import http
 from django.http.response import JsonResponse
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, Http404
 from rest_framework import serializers, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -26,4 +26,28 @@ class ExerciseList(APIView):
         
 
 class ExerciseDetail(APIView):
-    pass
+    def get_object(self, workout_id, exercise_id):
+        try:
+            return Exercise.objects.filter(workout_id__id=workout_id).get(id = exercise_id)
+        except Exercise.DoesNotExist:
+            raise Http404
+
+    def get(self, request, user_id, workout_id, exercise_id, format=None):
+        exercises = self.get_object(workout_id, exercise_id)
+        serializer = ExerciseSerializer(exercises)
+        return Response(serializer.data)
+    
+    def put(self, request, user_id, workout_id, exercise_id, format=None):
+        exercise = self.get_object(workout_id, exercise_id)
+        serializer = ExerciseSerializer(exercise, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, user_id, workout_id, exercise_id, format=None):
+        workout = self.get_object(workout_id, exercise_id)
+        workout.delete()
+        return Response("Exercise has successfully been deleted", status=status.HTTP_204_NO_CONTENT)
+    
+
