6f505f548d865d4e614456f4bd6c75678a0b26e4
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-06 10:04:36 +0100

    changed workouts of a user  url path to go through users/:user_id/workouts

--- devmuscles/users/urls.py
+++ devmuscles/users/urls.py
@@ -1,8 +1,10 @@
+from workouts.views import userWorkouts
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import UserList, UserDetail
 
 urlpatterns = [
     path('', UserList.as_view()),
-    path('<int:user_id>', UserDetail.as_view())
+    path('<int:user_id>', UserDetail.as_view()),
+    path('<int:user_id>/workouts', userWorkouts)
 ]
