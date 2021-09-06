e4ec10c2bf36be925691fcbb7ab8ce1bbe32e412
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-06 10:08:42 +0100

    added a route for a particular workout linked to a user

--- devmuscles/users/urls.py
+++ devmuscles/users/urls.py
@@ -1,4 +1,4 @@
-from workouts.views import userWorkouts
+from workouts.views import userWorkouts, userWorkout
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import UserList, UserDetail
@@ -6,5 +6,6 @@ from .views import UserList, UserDetail
 urlpatterns = [
     path('', UserList.as_view()),
     path('<int:user_id>', UserDetail.as_view()),
-    path('<int:user_id>/workouts', userWorkouts)
+    path('<int:user_id>/workouts', userWorkouts),
+    path('<int:user_id>/workouts/<int:workout_id>', userWorkout),
 ]
