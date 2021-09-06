3ffa1b81c63295f07c6ff7d4161a3106b42b5683
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-06 12:56:20 +0100

    removed some url patterns for clean up

--- devmuscles/workouts/urls.py
+++ devmuscles/workouts/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import WorkoutList, WorkoutDetail
 
 urlpatterns = [
-    path('', WorkoutList.as_view()),
-    path('<int:workout_id>', WorkoutDetail.as_view()),
+
 ]  
