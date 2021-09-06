0db4e8ffa25cf9242a32309f8005794237854e82
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-06 15:58:48 +0100

    auto generate token upon user creation

--- devmuscles/users/serializers.py
+++ devmuscles/users/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+
 class UserRegistrationSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True)
@@ -32,3 +37,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name')
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
