a99d48e3f706d2dd774f85596adb7a029e761ca8
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-06 12:57:09 +0100

    modified workout views to access the workouts only given a user

--- devmuscles/workouts/views.py
+++ devmuscles/workouts/views.py
@@ -8,12 +8,13 @@ from rest_framework.decorators import api_view
 
 # Create your views here.
 class WorkoutList(APIView):    
-    def get(self, request, format=None):
-            workouts = Workout.objects.all()
+    def get(self, request, user_id, format=None):
+            workouts = Workout.objects.filter(user_id__pk = user_id)
             serializer = WorkoutSerializer(workouts, many=True)
             return Response(serializer.data)
+
     
-    def post(self, request, format=None):
+    def post(self, request, user_id, format=None):
         serializer = WorkoutSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -21,39 +22,28 @@ class WorkoutList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class WorkoutDetail(APIView):
-    def get_object(self, workout_id):
+    def get_object(self, user_id, workout_id):
         try:
-            return Workout.objects.get(pk=workout_id)
+            return Workout.objects.filter(user_id__pk = user_id).get(id = workout_id)
         except Workout.DoesNotExist:
             raise Http404
 
-    def get(self, request, workout_id, format=None):
-        workout = self.get_object(workout_id)
+    def get(self, request, user_id, workout_id, format=None):
+        workout = self.get_object(user_id, workout_id)
         serializer = WorkoutSerializer(workout)
         return Response(serializer.data)
 
-    def put(self, request, workout_id, format=None):
-        workout = self.get_object(workout_id)
+    def put(self, request, user_id, workout_id, format=None):
+        workout = self.get_object(user_id, workout_id)
         serializer = WorkoutSerializer(workout, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, workout_id, format=None):
-        workout = self.get_object(workout_id)
+    def delete(self, request, user_id, workout_id, format=None):
+        workout = self.get_object(user_id, workout_id)
         workout.delete()
         return Response("Workout has successfully been deleted", status=status.HTTP_204_NO_CONTENT)
 
    
-@api_view(['GET'])
-def userWorkouts(self, user_id, format=None):
-    workouts = Workout.objects.filter(user_id__pk = user_id)
-    serializer = WorkoutSerializer(workouts, many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def userWorkout(self, user_id, workout_id, format=None):
-    workouts = Workout.objects.filter(user_id__pk = user_id).get(id = workout_id)
-    serializer = WorkoutSerializer(workouts)
-    return Response(serializer.data)
