29a49045487a2431f5dcb5cbf573a18849e13414
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-07 12:33:42 +0100

    customised login route

--- devmuscles/devmuscles/urls.py
+++ devmuscles/devmuscles/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from users.views import CustomObtainAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +24,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('exercises/', include('exercises.urls')),
     path('dates/', include('dates.urls')),
-    path('login/', obtain_auth_token, name='login'),
+    path('login/', CustomObtainAuthToken.as_view(), name='login'),
+
 ]
