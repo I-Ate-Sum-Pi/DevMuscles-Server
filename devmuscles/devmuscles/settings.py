fca6ba37fcb258820291787a40f90648fb2afa5e
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-07 10:54:22 +0100

    updated setting.py to allow hosts

--- devmuscles/devmuscles/settings.py
+++ devmuscles/devmuscles/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "devkey")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dev-muscles.herokuapp.com', 'localhost']
 
 
 # Application definition
