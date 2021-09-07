7bb1c23f0d24bbbb0a9df656fd2b754db127aff1
JawwadUddin <m.m.j.uddin12@gmail.com>
2021-09-07 17:55:09 +0100

    updated all url patterns to have str params instead of int since ids are now creted with uuid

--- devmuscles/users/urls.py
+++ devmuscles/users/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('', UserList.as_view()),
     path('<int:user_id>', UserDetail.as_view()),
     path('<int:user_id>/workouts', WorkoutList.as_view()),
-    path('<int:user_id>/workouts/<int:workout_id>', WorkoutDetail.as_view()),
-    path('<int:user_id>/workouts/<int:workout_id>/exercises', ExerciseList.as_view()),
-    path('<int:user_id>/workouts/<int:workout_id>/exercises/<int:exercise_id>', ExerciseDetail.as_view()),
+    path('<int:user_id>/workouts/<str:workout_id>', WorkoutDetail.as_view()),
+    path('<int:user_id>/workouts/<str:workout_id>/exercises', ExerciseList.as_view()),
+    path('<int:user_id>/workouts/<str:workout_id>/exercises/<str:exercise_id>', ExerciseDetail.as_view()),
     path('<int:user_id>/dates', DateList.as_view()),
-    path('<int:user_id>/dates/<int:date_id>', DateDetail.as_view()),    
+    path('<int:user_id>/dates/<str:date_id>', DateDetail.as_view()),    
 ]
